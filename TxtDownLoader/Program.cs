438b4bb1af23dd0c44c1a5d4a5c5820ae75c319c
hub-zjy1024 <851280963@qq.com>
2019-10-18 15:59:53 +0800

    add GlobalExHandler

--- TxtDownLoader/Program.cs
+++ TxtDownLoader/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using TxtDownLoader.utils;
 
 namespace TxtDownLoader
 {
@@ -14,6 +15,7 @@ namespace TxtDownLoader
         [STAThread]
         static void Main()
         {
+            GlobalExHandler.init();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
